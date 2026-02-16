3dbe0fa502696c85a82062c4f3589b7e1ca75345
Boris Glavic <lordpretzel@gmail.com>
2026-02-16 09:48:40 -0600

    syntax highlighting and indentation rules

--- radb-mode.el
+++ radb-mode.el
@@ -32,11 +32,193 @@
 ;; ********************************************************************************
 ;; IMPORTS
 
+(require 'treesit)
+
+(declare-function treesit-parser-create "treesit.c")
+
 ;; ********************************************************************************
 ;; CUSTOM
+(defvar radb-mode--treesit-settings
+  (treesit-font-lock-rules
+   :default-language 'radb
+
+   :feature 'string
+   :override t
+   '((STRING) @font-lock-string-face)
+
+   :feature 'string
+   :override t
+   '((NUMBER (INTEGER)) @font-lock-constant-face)
+
+   :feature 'attribute
+   :override t
+   '((attr) @font-lock-type-face)
+
+   :feature 'view-name
+   :override t
+   '((view viewname: (IDENTIFIER) @font-lock-variable-name-face))
+
+   :feature 'function-calls
+   :override t
+   '((agg_function) @font-lock-function-name-face)
+
+   :feature 'function-calls
+   :override t
+   '((agg_function) @font-lock-function-name-face)
+
+   :feature 'table-name
+   :override t
+   '((tableaccess tablename: (IDENTIFIER) @font-lock-variable-name-face))
+
+   :feature 'keyword
+   :override t
+   '((DEFAS) @font-lock-keyword-face
+     (PROJECT) @font-lock-keyword-face
+     (SELECT) @font-lock-keyword-face
+     (RENAME) @font-lock-keyword-face
+     (KWJOIN) @font-lock-keyword-face
+     (KWCROSS) @font-lock-keyword-face
+     (KWUNION) @font-lock-keyword-face
+     (KWDIFFERENCE) @font-lock-keyword-face
+     (KWINTERSECTION) @font-lock-keyword-face
+     (AGGREGATION) @font-lock-keyword-face
+     (LIST) @font-lock-keyword-face
+     (SOURCE) @font-lock-keyword-face
+     (QUIT) @font-lock-keyword-face
+     (SAVE) @font-lock-keyword-face
+     (SQLEXEC) @font-lock-keyword-face)
+
+   :feature 'delimiter
+   :override t
+   '((["," ";"]) @font-lock-delimiter-face)
+
+   :feature 'bracket
+   '((["(" ")" "{" "}"]) @font-lock-bracket-face)
+
+   :feature 'operator
+   '((["=" "<" ">" "<=" ">="]) @font-lock-operator-face)
+
+   :feature 'string
+   :override t
+   '((COMMENT) @font-lock-comment-face)
+   )
+  "Tree-sitter font-lock settings.")
+
+(defcustom radb-mode-indent-offset
+  4
+  "Number of spaces for one level of indentation."
+  :group 'radb-mode
+  :type 'number)
+
+(defvar radb-mode--indent-rules
+  `((radb
+       ((node-is ")") parent 0)
+       ((node-is "view") parent-bol 0)
+       ((node-is "query") parent-bol 0)
+       ((node-is "utility") parent-bol 0)
+       ((and (parent-is ,(rx (or "projection"
+                         "selection"
+                         "rename"
+                         "aggregation")))
+             (node-is "comment")) parent radb-mode-indent-offset)
+       ((and (parent-is ,(rx (or "union"
+                         "join"
+                         "difference"
+                         "intersection")))
+             (node-is "comment")) parent 0)
+       ((node-is "comment") column-0 0)
+       ((field-is "leftinput") parent 0)
+       ((field-is "rightinput") first-sibling 0)
+       ((node-is "KWUNION") first-sibling 0)
+       ((node-is "KWJOIN") first-sibling 0)
+       ((node-is "KWCROSS") first-sibling 0)
+       ((node-is "KWDIFFERENCE") first-sibling 0)
+       ((node-is "KWINTERSECTION") first-sibling 0)
+       ((field-is "^input$") parent radb-mode-indent-offset)
+       ((match nil "selection" "condition" nil nil) parent 9)
+       ((match nil "join" "condition" nil nil) parent 5)
+       ((query "(projection ((_) @ident))") parent 10)
+       ((query "(rename ((_) @ident))") parent 9)
+       ((catch-all) column-0 0)))
+  "Tree-sitter indentation rules.")
 
 ;; ********************************************************************************
 ;; FUNCTIONS
+(defun radb-mode--ts-imenu-name (node)
+  "Get name for imenu element for treesit NODE."
+  (pcase (treesit-node-type node)
+    ("view"
+     (treesit-node-text
+      (treesit-search-subtree
+       (treesit-node-child-by-field-name node "viewname")
+       "IDENTIFIER")))
+    ("utility"
+     (replace-regexp-in-string "\\\\" ""
+     (treesit-node-text
+      (treesit-node-child-by-field-name
+       (treesit-node-child node 0)
+       "command"))))
+    (t
+     (treesit-node-text node))))
+
+(defun radb-mode--treesit-defun-name (node)
+  "Treesit node types NODE defined to be defuns."
+  (pcase (treesit-node-type node)
+    ((or "view" "utility")
+     (radb-mode--treesit-is-imenu-entry node))
+    (_ nil)))
+
+(defun radb-mode--treesit-is-imenu-entry (node)
+  "Return non-nil if NODE is should be an imenu entry."
+  (pcase (treesit-node-type node)
+    ("view"
+     t)
+    ("utility"
+     t)
+    ("query"
+     (not (treesit-parent-until node "view")))))
+
+;; ********************************************************************************
+;; MODE DEFINITION
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.ra\\'" . radb-mode))
+
+;;;###autoload
+(define-derived-mode radb-mode prog-mode "radb"
+  "Major mode for editing radb relational algebra files using the tree-sitter library.
+
+\\{radb-mode-map}"
+  ;; :syntax-table python-mode-syntax-table
+  (when (treesit-ready-p 'radb)
+    (treesit-parser-create 'radb)
+
+    ;; font lock
+    (setq-local treesit-font-lock-feature-list
+                '((comment keyword view-name table-name)
+                  (string function-calls attribute)
+                  (number operator delimiter)
+                  (bracket)))
+    (setq-local treesit-font-lock-settings
+                radb-mode--treesit-settings)
+
+    (setq-local treesit-simple-imenu-settings
+                `(("Query" "\\`query\\'" radb-mode--treesit-is-imenu-entry radb-mode--ts-imenu-name)
+                  ("Utility" "\\`utility\\'" radb-mode--treesit-is-imenu-entry radb-mode--ts-imenu-name)
+                  ("View definition" "\\`view\\'" radb-mode--treesit-is-imenu-entry radb-mode--ts-imenu-name)))
+
+    ;; Navigation.
+    (setq-local treesit-defun-type-regexp
+                (regexp-opt '("query"
+                              "view"
+                              "utility")))
+    (setq-local treesit-defun-name-function #'radb-mode--treesit-defun-name)
+
+    ;; indentation
+    (setq-local treesit-simple-indent-rules radb-mode--indent-rules)
+    ;; activate tree sitter
+    (treesit-major-mode-setup)))
+
 
 (provide 'radb-mode)
 ;;; radb-mode.el ends here
